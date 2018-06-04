beb17322bf798dedc149907803ad7b3f8a883235
Huet Arnaud <arno.vkh@gmail.com>
2018-06-04 14:46:30 +0400

    corrected test

--- todo/e2e/src/app.e2e-spec.ts
+++ todo/e2e/src/app.e2e-spec.ts
@@ -3,20 +3,20 @@ import {element} from 'protractor';
 describe('todo list', () => {
   it('should be able to add and remove a todo item', async function () {
       browser.get('http://localhost:4200');
-      const todoItems = element.all(by.css('li.todo-item'));
+      const todoItems = element.all(by.css('#list-1 li.todo-item'));
       const initialTodoItemsCount = await todoItems.count();
-      element(by.css('.add-item')).sendKeys('automated ui tests');
-      element(by.css('button')).click();
+      element(by.css('#input-1.add-item')).sendKeys('automated ui tests');
+    element(by.css('#btn-1')).click();
 
-      expect(todoItems
-        .count()).toEqual(initialTodoItemsCount + 1);
+    expect(todoItems
+      .count()).toEqual(initialTodoItemsCount + 1);
 
-      expect(todoItems.get(initialTodoItemsCount).element(by.css('.todo-item-description')).getText()).toEqual('automated ui tests');
-      element.all(by.css('button.remove-item'))
-        .get(initialTodoItemsCount).click();
-      expect(todoItems.count()).toEqual(initialTodoItemsCount);
-      const actual = todoItems.filter(e => e.getText() === 'automated ui tests').count();
-      expect(actual).toEqual(0);
-    }
-  );
+    expect(todoItems.get(initialTodoItemsCount).element(by.css('#list-1  .todo-item-description')).getText()).toEqual('automated ui tests');
+    element.all(by.css('#list-1 button.remove-item'))
+      .get(initialTodoItemsCount).click();
+    expect(todoItems.count()).toEqual(initialTodoItemsCount);
+    const actual = todoItems.filter(e => e.getText() === 'automated ui tests').count();
+    expect(actual).toEqual(initialTodoItemsCount);
+  }
+);
 });
